1d874959cadda9494ea74cb0a1bcad63c2e80a5d
Ariana <1729ariana@gmail.com>
2022-07-09 18:28:12 +0000

    oops setup file had hardcoded stuff lol

--- backend/setup_test.py
+++ backend/setup_test.py
@@ -3,7 +3,7 @@ import random
 
 pcl = [310145, 650145, 650147, 650146, 569933, 310223, 650160, 520147, 521147]
 pcd = {}
-for n,p in []:#enumerate(pcl):
+for n,p in enumerate(pcl):
     r = requests.get("http://127.0.0.1:8000/user/create",params={
         "username":f"testUser{n}",
         "password":"password",
@@ -13,7 +13,7 @@ for n,p in []:#enumerate(pcl):
     print(r.json())
     pcd[p] = r.json()["id"]
 
-pcd = {310145: '62c9c64af9478e818bba151d', 650145: '62c9c64af9478e818bba151e', 650147: '62c9c64bf9478e818bba151f', 650146: '62c9c64bf9478e818bba1520', 569933: '62c9c64cf9478e818bba1521', 310223: '62c9c64cf9478e818bba1522', 650160: '62c9c64df9478e818bba1523', 520147: '62c9c64df9478e818bba1524', 521147: '62c9c64ef9478e818bba1525'}
+# pcd = {310145: '62c9c64af9478e818bba151d', 650145: '62c9c64af9478e818bba151e', 650147: '62c9c64bf9478e818bba151f', 650146: '62c9c64bf9478e818bba1520', 569933: '62c9c64cf9478e818bba1521', 310223: '62c9c64cf9478e818bba1522', 650160: '62c9c64df9478e818bba1523', 520147: '62c9c64df9478e818bba1524', 521147: '62c9c64ef9478e818bba1525'}
 
 items = {}
 
